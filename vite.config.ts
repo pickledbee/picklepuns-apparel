c48e7c2316d3a9bf4353acedcf9dbfc2a11c14ff
pickledbee <stradman100@gmail.com>
2025-10-24 14:59:41 -0500

    Fixed GitHub Pages blank screen with correct Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/picklepuns-apparel/',  // 👈 this line fixes the blank screen
+  base: '/picklepuns-apparel/', // 👈 This line fixes the blank white screen on GitHub Pages
 })
+
