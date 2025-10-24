69cc54e3600a8bd00e8d8d8c3187ce62f6c91147
pickledbee <stradman100@gmail.com>
2025-10-24 15:07:41 -0500

    Force add dist folder for GitHub Pages visibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/picklepuns-apparel/', // 👈 This line fixes the blank white screen on GitHub Pages
+  base: '/picklepuns-apparel/',
 })
-
